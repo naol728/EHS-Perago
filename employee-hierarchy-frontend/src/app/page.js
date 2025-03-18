bca7e14c224ac5dd40c6aac9dfdad3e3b43167f0
naol <naolmeseret22@gmail.com>
2025-03-18 21:03:03 +0300

    inistal commit

---
+++ employee-hierarchy-frontend/src/app/page.js
@@ -0,0 +1,10 @@
+import Appbar from "../../components/Appbar";
+import Button from "../../components/Button";
+
+export default function Home() {
+  return (
+    <div className="">
+      this is home page 
+    </div>
+  );
+}
